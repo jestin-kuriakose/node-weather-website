5fdf05890cf8082885358686bec4813d6777fd50
jestin-kuriakose <jestin.kuriakose@teslaelectronics.ca>
2021-08-25 15:01:26 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,4 @@
-console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -22,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     messageOne.textContent = 'Loading..'
     messageTwo.textContent = ''
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messageOne.textContent = data.error
